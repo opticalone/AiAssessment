b8707a128ecddd0b6370f759277df2bcb22341f0
Michael Kaplor <s179059@students.aie.edu.au>
2018-02-13 16:07:28 -0800

    added volumetric light

---
+++ Assets/uiSpin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiSpin : MonoBehaviour {
+
+
+    public float spinSpeed= 0f;
+    
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+       
+            
+            transform.Rotate(new Vector3(0f, spinSpeed, 0f));
+        
+        
+	}
+}

4d7a3928aea6f59f122e9120d5d7992334d4f3b6
Michael Kaplor <s179059@students.aie.edu.au>
2018-02-14 08:43:32 -0800

    final scene setup

--- Assets/uiSpin.cs
+++ Assets/uiSpin.cs
@@ -6,13 +6,19 @@ public class uiSpin : MonoBehaviour {
 
 
     public float spinSpeed= 0f;
-    
+
+    float input;
+    void Update()
+    {
+        input = Input.GetAxis("Vertical") *spinSpeed;
+    }
+
 	// Update is called once per frame
 	void FixedUpdate ()
     {
        
-            
-            transform.Rotate(new Vector3(0f, spinSpeed, 0f));
+        
+            transform.Rotate(new Vector3(0f, input, 0f));
         
         
 	}
